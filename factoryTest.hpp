86007f3483d734482bb65e57aeeb8de14a264b5d
awang923 <awang144@ucr.edu>
2020-11-27 21:48:14 -0800

    add test

---
+++ factoryTest.hpp
@@ -0,0 +1,17 @@
+#ifndef _FACTORYTEST__
+#define _FACOTRYTEST__
+#include "gtest/gtest,h"
+#include "base.hpp"
+#include"op.hpp"
+#include "sub.hpp"
+#include "add.hpp"
+#include "mult.hpp"
+#include "div.hpp"
+#include "rand.hpp"
+#include "pow.hpp"
+#include "expFactory.hpp"
+
+
+
+
+#endif //_FACTORYTEST__
